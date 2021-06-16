7f57354da9661fb43ee7899a28bf8614786f1630
KainTheReaper <kainthereaper@gmail.com>
2021-06-16 12:42:52 -0700

    Initial Upload
    ITS ALIVE!

---
+++ Assets/Scripts/DeepLinkTest.cs
@@ -0,0 +1,122 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeepLinkTest : MonoBehaviour
+{
+
+    public GameObject TwitchBroadcastGameButton;
+    public GameObject OpenTwitchButton;
+    public GameObject GameInputField;
+    public InputField testInputField;
+    private int broadcastGameID = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TwitchBroadcastGameButton = GameObject.Find("TwitchDeepLinkButton");
+        GameInputField = GameObject.Find("GameIDInputField");
+        OpenTwitchButton = GameObject.Find("OpenTwitchButton");
+
+
+        if (isTwitchInstalled())
+        {
+            Debug.Log("Twitch App Installed!");
+            TwitchBroadcastGameButton.SetActive(true);
+            GameInputField.SetActive(true);
+            OpenTwitchButton.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("Twitch App Not Found!");
+            TwitchBroadcastGameButton.SetActive(false);
+            GameInputField.SetActive(false);
+            OpenTwitchButton.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private bool isTwitchInstalled()
+    {
+        bool isInstalled = false;
+#if UNITY_ANDROID
+            //Bundle ID for the Twitch APP
+            string bundleId = "tv.twitch.android.app";
+            
+            //Nasty C# into java reflection
+            AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            AndroidJavaObject currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+            AndroidJavaObject packageManager = currentActivity.Call<AndroidJavaObject>("getPackageManager");
+            AndroidJavaObject launchIntent = packageManager.Call<AndroidJavaObject>("getLaunchIntentForPackage",bundleId);
+                        
+            //Launch play store if Twitch is not installed
+            if (launchIntent == null)
+            {
+                Application.OpenURL("market://details?id=" + bundleId);
+            }
+            else
+            {
+                isInstalled = true;
+            }
+
+            //Garbage clean up as we will never use these objects again
+            unityPlayer.Dispose();
+            currentActivity.Dispose();
+            packageManager.Dispose();
+            launchIntent.Dispose();
+#endif
+#if UNITY_IOS
+            var iosAppLink = "twitch://open/";
+            if (IOSCanOpenURL.CheckUrl(iosAppLink))
+            {
+                isInstalled = true;
+            }
+            else
+            {
+                Application.OpenURL("itms-apps://itunes.apple.com/us/app/twitch-live-game-streaming/id460177396");
+            }
+#endif
+        return isInstalled;
+    }
+
+    /// <summary>
+    /// There's a better way to do this.
+    /// </summary>
+    public void GatherText()
+    {
+        SetGameID(testInputField.text);
+    }
+
+    public void OpenTwitch()
+    {
+        Application.OpenURL("twitch://open/");
+    }
+
+    public void SetGameID(string gameID)
+    {
+
+        int number;
+
+        bool isParsable = int.TryParse(gameID, out number);
+
+        if (isParsable)
+        {
+            Debug.Log("Game ID is: " + number);
+            broadcastGameID = number;
+        }
+        else
+        {
+            Debug.Log("Could not be parsed.");
+        }
+    }
+
+    public void BroadcastGame()
+    {
+        Application.OpenURL("twitch://broadcast?game_id=" + broadcastGameID.ToString());
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# UnityDeeplink
-A Unity project showcasing how to deeplink on Android and IOS
+# Twitch Unity Deeplink Sample
+
+## What does this do
+
+This is an example which shows you how to check if twitch is installed on both Android and IOS in Unity
+And how to launch a broadcast for a particular game id using the Twitch app's deeplink functionality
+
+## How to use
+
+Depending on which mobile platform and os version you are targeting you may need to make additional changes.
+See below for additional details for a given platform.
+
+### UI interface
+
+The open twitch button will simply launch the twitch app. (Buttons will disable if Twitch is not installed)
+
+The input text box is used to enter your game id. Getting the game id can be done via this api: https://dev.twitch.tv/docs/api/reference#get-games
+Once the game id has been entered push the "Broadcast" button to start twitch streaming via the deeplink functionality.
+
+### Android
+
+On Unity 2021 nothing further is required for this to function
+
+Unity 2020 will require:
+
+`<meta-data android:name="unityplayer.UnityActivity" android:value="true" />`
+
+Unity 2019 and older will require (Note this was not tested but should work):
+
+```<activity android:name="com.unity3d.player.UnityPlayerNativeActivity"
+          android:label="@string/app_name">
+  <intent-filter>
+    <action android:name="android.intent.action.MAIN" />
+    <category android:name="android.intent.category.LAUNCHER" />
+    <category android:name="android.intent.category.LEANBACK_LAUNCHER" />
+  </intent-filter>
+  <meta-data android:name="unityplayer.UnityActivity" android:value="true"
+  />
+  <meta-data
+ android:name="unityplayer.ForwardNativeEventsToDalvik"android:value="false"
+  />
+</activity>```
+
+### IOS
+
+You must add LSApplicationQueriesSchemes to your app's Info.plist
+
+To do this you must export to XCODE to manually edit the plist or alternatively do it programatically
+An example of how to do this programtically can be found in Nrjwolf's documentation (Link below)
+
+More documentation can be found here: https://developer.apple.com/documentation/uikit/uiapplication/1622952-canopenurl?language=objc
+
+
+## Acknowledgments and Appendix
+
+Specials thanks to Nrjwolf for creating UNITY-IOS-CAN-OPEN located here: https://github.com/Nrjwolf/unity-ios-can-open-url
